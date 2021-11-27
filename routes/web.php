9338d3f85377d597e2e72e1257caa55d8f5f4f70
GuillermoTejera <guilletejera@gmail.com>
2021-11-27 19:56:31 -0300

    Mejora ruteo, traductor de google

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 if(isset($router)) {
     // Si la pages existe la redirige
     $router->get('/', 'controllers\Home@index');
-    $router->get('/pregunta/(\d+)', 'controllers\Home@pregunta');
+    $router->get('/pregunta/facil/(\d+)', 'controllers\Home@pregunta');
+    $router->get('/pregunta/intermedio/(\d+)', 'controllers\Home@pregunta');
+    $router->get('/pregunta/deficil/(\d+)', 'controllers\Home@pregunta');
     $router->get('/puntuacion', 'controllers\Home@puntuacion');
 
     // Sino se mostrar la page 404
