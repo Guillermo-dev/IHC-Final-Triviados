d09ca2e8ff1cf6bd3782fae07d5ed47f63e626e9
GuillermoTejera <guilletejera@gmail.com>
2021-11-30 23:13:47 -0300

    Mejora de rutas

--- controllers/Home.php
+++ controllers/Home.php
@@ -5,10 +5,34 @@ namespace controllers;
 abstract class Home {
 
     public static function index(): void {
+        session_destroy();
         echo file_get_contents('src/pages/home/home.html');
     }
 
-    public static function pregunta(): void {
+    public static function preguntaEasy(): void {
+        if (!isset($_SESSION['dificultad'])) {
+            $_SESSION['dificultad'] = 'easy';
+        } else if ($_SESSION['dificultad'] != 'easy') {
+            header('Location: /');
+        }
+        echo file_get_contents('src/pages/pregunta/pregunta.html');
+    }
+
+    public static function preguntaMedium(): void {
+        if (!isset($_SESSION['dificultad'])) {
+            $_SESSION['dificultad'] = 'medium';
+        } else if ($_SESSION['dificultad'] != 'medium') {
+            header('Location: /');
+        }
+        echo file_get_contents('src/pages/pregunta/pregunta.html');
+    }
+
+    public static function preguntaHard(): void {
+        if (!isset($_SESSION['dificultad'])) {
+            $_SESSION['dificultad'] = 'hard';
+        } else if ($_SESSION['dificultad'] != 'hard') {
+            header('Location: /');
+        }
         echo file_get_contents('src/pages/pregunta/pregunta.html');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 if(isset($router)) {
     // Si la pages existe la redirige
     $router->get('/', 'controllers\Home@index');
-    $router->get('/pregunta/easy', 'controllers\Home@pregunta');
-    $router->get('/pregunta/medium', 'controllers\Home@pregunta');
-    $router->get('/pregunta/hard', 'controllers\Home@pregunta');
+    $router->get('/pregunta/easy', 'controllers\Home@preguntaEasy');
+    $router->get('/pregunta/medium', 'controllers\Home@preguntaMedium');
+    $router->get('/pregunta/hard', 'controllers\Home@preguntaHard');
     $router->get('/puntuacion', 'controllers\Home@puntuacion');
 
     // Sino se mostrar la page 404
