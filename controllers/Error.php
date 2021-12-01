677b50b0ac43947a24a185e9e0da7c7228036840
GuillermoTejera <guilletejera@gmail.com>
2021-12-01 16:06:57 -0300

    Mejora de pages de error

--- controllers/Error.php
+++ controllers/Error.php
@@ -5,13 +5,15 @@ namespace controllers;
 abstract class Error {
 
     public static function page404(): void {
-        session_destroy();
+        if(isset($_SEESION))
+            session_destroy();
         header('HTTP/1.0 404 Not Found');
         echo file_get_contents('src/pages/404/404.html');
     }
 
     public static function page500(): void {
-        session_destroy();
+        if(isset($_SEESION))
+            session_destroy();
         header('HTTP/1.0 500 Internal Server Error');
         echo file_get_contents('src/pages/500/500.html');
     }
